42b932bd3a4a7f12e3a0915f5e4f05a02090750d
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-09-01 14:57:27 +0300

    Update Input.tsx

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,27 +1,21 @@
-import * as React from "react";
-
+import React from "react";
 import { cn } from "../utils";
 
-type InputProps = {
-  className?: string;
-  type?: string;
-};
-
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
-    );
+export const Input = React.forwardRef<
+  HTMLInputElement,
+  {
+    [key: string]: string | any;
   }
-);
-Input.displayName = "Input";
-
-export { Input };
+>((props, ref) => {
+  return (
+    <input
+      ref={ref}
+      type={props.type}
+      className={cn(
+        "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+        props.className
+      )}
+      {...props}
+    />
+  );
+});
