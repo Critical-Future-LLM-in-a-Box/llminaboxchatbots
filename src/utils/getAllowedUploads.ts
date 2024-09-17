cb73cce951f4e79c84fd2258a89215656c53256d
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-09-17 08:05:14 +0300

    utility functions

---
+++ src/utils/getAllowedUploads.ts
@@ -0,0 +1,22 @@
+import { ChatData } from "@/context";
+
+export const getAllowedUploads = async (
+  chatData: ChatData,
+  dispatch: Function
+): Promise<void> => {
+  const response = await fetch(
+    `${chatData.config.apiHost}/api/v1/public-chatbotConfig/${chatData.config.chatflowid}`
+  ).catch((error) => {
+    throw new Error(`Error fetching chatbot config: ${error}`);
+  });
+
+  if (!response.ok) {
+    throw new Error(`Error fetching chatbot config: ${response.statusText}`);
+  }
+
+  const { isApiAcceptingimage: image, isApiAcceptingVoice: voice } =
+    await response.json();
+
+  dispatch({ type: "SET_API_ACCEPTING_VOICE", payload: voice });
+  dispatch({ type: "SET_API_ACCEPTING_IMAGE", payload: image });
+};
---
+++ src/utils/getVoice.ts
@@ -0,0 +1,56 @@
+import localforage from "localforage";
+import { ChatData, Message } from "@/context";
+import { request } from "@/utils/request";
+
+export const fetchVoice = async (chatData: ChatData, message: Message) => {
+  const { file_url } = await request<{ file_url: string }>({
+    url: "http://51.20.131.200/get_tts",
+    options: {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": "G7x9mVt2Q5bK8Jp4S1Zc"
+      },
+      body: JSON.stringify({
+        text: message.content,
+        voice: chatData.config?.voiceName,
+        id: chatData.config?.name
+      })
+    }
+  }).catch((err) => {
+    throw new Error(err);
+  });
+
+  const audioBlob = await request<Blob>({ url: file_url }).catch((err) => {
+    throw new Error(err);
+  });
+
+  const mp3Blob = new Blob([audioBlob], { type: "audio/mpeg" });
+
+  return URL.createObjectURL(mp3Blob);
+};
+
+export const getVoice = async (chatData: ChatData, message: Message) => {
+  const ttsCacheString: string =
+    (await localforage.getItem("llminabox_tts")) ?? "{}";
+
+  let ttsCacheJson: { [key: string]: string };
+  let localVoice: string;
+
+  ttsCacheJson = JSON.parse(ttsCacheString);
+  localVoice = ttsCacheJson[message.timestamp];
+
+  if (localVoice) {
+    return new Audio(localVoice);
+  }
+
+  const audioUrl = await fetchVoice(chatData, message).catch((err) => {
+    throw new Error(err);
+  });
+
+  if (audioUrl) {
+    ttsCacheJson[message.timestamp] = audioUrl;
+    await localforage.setItem("llminabox_tts", JSON.stringify(ttsCacheJson));
+    return new Audio(audioUrl);
+  }
+};
