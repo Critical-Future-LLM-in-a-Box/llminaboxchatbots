c01b13d62e10f675a07e6988caac69c3c2ffaca9
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-09-17 08:04:02 +0300

    Delete ChatbotFull.tsx

--- src/components/ChatbotFull.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Chatbot from "./Chatbot";
-import { config } from "../index";
-
-const ChatbotFull: React.FC<{ config: config }> = ({ config }) => {
-  return <Chatbot config={config} />;
-};
-
-export default ChatbotFull;
