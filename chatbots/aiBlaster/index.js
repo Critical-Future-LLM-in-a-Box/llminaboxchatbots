dc48153c796ff12e21b12d3073c1173c1f417379
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-11-06 18:17:26 +0200

    Create index.js

---
+++ chatbots/aiBlaster/index.js
@@ -0,0 +1,19 @@
+(async () => {
+  const apiHost = "https://llm.criticalfutureglobal.com";
+  const chatflowid = "ef3dfcbb-0a6d-419c-a42c-31614ff7308e";
+  const themeFront = "#1d0462";
+  const themeBack = "#e2deea";
+
+  const fullchatbot = document.createElement("flowise-fullchatbot");
+  document.querySelector("llminabox").appendChild(fullchatbot);
+
+  (await import("../base/web.js")).default.initFull(
+    (await import("./config.js")).generateConfig({
+      apiHost,
+      chatflowid,
+      themeFront,
+      themeBack,
+      avatarIcon,
+    })
+  );
+})();
