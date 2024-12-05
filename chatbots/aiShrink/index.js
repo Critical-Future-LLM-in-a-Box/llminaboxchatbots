2488f16876648e8a8c39b9c576961c8a21243134
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2024-12-05 11:42:18 +0200

    Update index.js

--- chatbots/aiShrink/index.js
+++ chatbots/aiShrink/index.js
@@ -1,6 +1,6 @@
 (async () => {
   const apiHost = "https://llminabox.criticalfutureglobal.com";
-  const chatflowid = "59abed11-4357-4f20-bdd4-9169510aeeef";
+  const chatflowid = "be69f112-ee19-41e0-8550-555b35b4d113";
   const themeFront = "#1d0462";
   const themeBack = "#e2deea";
 
