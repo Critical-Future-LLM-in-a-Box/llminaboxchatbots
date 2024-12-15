289ced0768bbb17165e1a8bf5619bf198bd21a98
Mohamed Tharwat <mohamed.tharwat.alazab@gmail.com>
2024-12-15 04:21:50 +0200

    fix unmounting

--- src/index.tsx
+++ src/index.tsx
@@ -7,48 +7,38 @@ import CssBaseline from "@mui/material/CssBaseline";
 import ChatbotFull from "@/components/ChatbotFull";
 import ChatbotBubble from "@/components/ChatbotBubble";
 
-let chatbotRootElement: HTMLElement | null =
-  document.querySelector("#llminabox");
 let chatbotReactRoot: ReactDOM.Root | null = null;
-
-export function initChatbot(config: Config = {}): void {
-  let Chatbot = ChatbotFull;
-
-  if (!chatbotRootElement) {
-    chatbotRootElement = document.createElement("div");
-    chatbotRootElement.id = "llminabox";
-    document.body.appendChild(chatbotRootElement);
-    Chatbot = ChatbotBubble;
-  }
-
-  chatbotRootElement.style.cssText = `
-      display: block;
-      width: 100%;
-      height: 100%;
-      overflow: hidden;
-    `;
-
-  if (!chatbotReactRoot) {
-    chatbotReactRoot = ReactDOM.createRoot(chatbotRootElement);
-  }
-
-  chatbotReactRoot.render(
-    <ContextProvider config={config}>
-      <CssBaseline />
-      <Chatbot />
-    </ContextProvider>
-  );
-}
 
 export function destroyChatbot(): void {
   if (chatbotReactRoot) {
     chatbotReactRoot.unmount();
     chatbotReactRoot = null;
   }
+}
 
-  if (chatbotRootElement) {
-    chatbotRootElement.style.cssText = `
-      display: none;
-    `;
+export function initChatbot(config: Config = {}): void {
+  const llminaboxFull = document.getElementById("llminabox-full");
+  const llminaboxBubble = document.getElementById("llminabox-bubble");
+
+  if (llminaboxFull) {
+    renderChatbot(llminaboxFull, ChatbotFull, config);
+  } else if (llminaboxBubble) {
+    renderChatbot(llminaboxBubble, ChatbotBubble, config);
+  } else {
+    console.warn("No valid chatbot container found.");
   }
+}
+
+function renderChatbot(
+  container: HTMLElement,
+  ChatbotComponent: React.FC,
+  config: Config
+): void {
+  chatbotReactRoot = ReactDOM.createRoot(container);
+  chatbotReactRoot.render(
+    <ContextProvider config={config}>
+      <CssBaseline />
+      <ChatbotComponent />
+    </ContextProvider>
+  );
 }
