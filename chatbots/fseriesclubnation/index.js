c30d6e4ea0dc0772cd65dcee19157ffe8ff13493
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2025-02-05 21:26:21 +0200

    Update index.js

--- chatbots/fseriesclubnation/index.js
+++ chatbots/fseriesclubnation/index.js
@@ -1,42 +1,40 @@
-import { initChatbot } from "https://critical-future-llm-in-a-box.github.io/llminaboxchatbots/dist/llminaboxChat.js";
+await (async () => {
+  const apiHost = "https://llm.criticalfutureglobal.com";
+  const chatflowid = "713ac6ba-4a32-40a0-9408-89478be4337b";
+  const themeFront = "#212121";
+  const themeBack = "#e2deea";
+  const avatarIcon =
+    "https://critical-future-llm-in-a-box.github.io/llminaboxchatbots/Avatars/fs/joe.png";
+  const avatarImage =
+    "https://critical-future-llm-in-a-box.github.io/llminaboxchatbots/Avatars/fs/joe.gif";
+  const avatarVideo =
+    "https://critical-future-llm-in-a-box.github.io/llminaboxchatbots/Avatars/fs/joe.mp4";
 
-const config = {
-  apiHost: "https://llminabox.criticalfutureglobal.com",
-  chatflowId: "0f6e4479-ba3d-4a34-b0cb-be96f269a24c",
-  assistant: {
-    name: "Joe",
-    description: "Fseries AI Assistant",
-    welcomeMessage: "Hello! How can I help you?",
-    voice: {
-      name: "en-US-AndrewNeural",
-      apiKey: "G7x9mVt2Q5bK8Jp4S1Zc",
-      apiHost: "https://tts.criticalfutureglobal.com/get_tts"
-    },
-    avatar: {
-      staticUrl:
-        "https://critical-future-llm-in-a-box.github.io/llminaboxchatbots/Avatars/fs/joe.png",
-      liveUrl:
-        "https://critical-future-llm-in-a-box.github.io/llminaboxchatbots/Avatars/fs/joe.gif",
-      videoUrl:
-        "https://critical-future-llm-in-a-box.github.io/llminaboxchatbots/Avatars/fs/joe.mp4"
-    }
-  },
-  ui: {
-    sidebar: true,
-    foregroundColor: "#e2deea",
-    backgroundColor: "#212121",
-    backgroundColorBody: "#54545480"
+  if (location.href === "https://fseriesclubnation.com/") {
+    const chatbot = document.createElement("flowise-fullchatbot");
+    document.querySelector("#header-outer").after(chatbot);
+
+    (await import("../base/web.js")).default.initFull(
+      (await import("./config.js")).generateConfig({
+        apiHost,
+        chatflowid,
+        themeFront,
+        themeBack,
+        avatarIcon
+      })
+    );
+
+    const utils = await import("../base/utils.js");
+
+    await utils.createNewSection(chatbot, {
+      avatarImage,
+      avatarVideo,
+      themeFront,
+      themeBack
+    });
+
+    // await utils.urlPreview(chatbot);
+
+    await utils.ttsSupport(chatbot, "en-US-AndrewNeural");
   }
-};
-
-if (location.href === "https://fseriesclubnation.com/") {
-  const container = document.createElement("llminabox");
-  document.querySelector("#header-outer").after(container);
-  container.style.cssText = `
-    display: block;
-    width: 100%;
-    height: 100%;
-    max-height: 70vh;
-  `;
-  initChatbot(config, container, "full");
-}
+})();
