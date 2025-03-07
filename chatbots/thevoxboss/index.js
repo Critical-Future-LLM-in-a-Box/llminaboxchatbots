9999d1e24194995985bbd65ba86bb37cd0da7ec3
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2025-03-08 01:37:24 +0200

    Update index.js

--- chatbots/thevoxboss/index.js
+++ chatbots/thevoxboss/index.js
@@ -1,42 +1,25 @@
+import { initChatbot } from "../../dist/llminaboxChat.js";
 (async () => {
-  const apiHost = "https://llm.criticalfutureglobal.com";
-  const chatflowid = "ef3dfcbb-0a6d-419c-a42c-31614ff7308e";
-  const themeFront = "#1d0462";
-  const themeBack = "#e2deea";
-  const avatarIcon =
-    "https://critical-future-llm-in-a-box.github.io/llminaboxchatbots/Avatars/blaster/blaster.png";
-  const avatarImage =
-    "https://critical-future-llm-in-a-box.github.io/llminaboxchatbots/Avatars/blaster/blaster.gif";
-  const avatarVideo =
-    "https://critical-future-llm-in-a-box.github.io/llminaboxchatbots/Avatars/blaster/blaster.mp4";
-
-  const llminaboxWrapper = await (
-    await import("../base/utils.js")
-  ).waitForElement("llminabox");
-
-  const chatbot = document.createElement("flowise-fullchatbot");
-  llminaboxWrapper.appendChild(chatbot);
-
-  (await import("../base/web.js")).default.initFull(
-    (await import("./config.js")).generateConfig({
-      apiHost,
-      chatflowid,
-      themeFront,
-      themeBack,
-      avatarIcon,
-    })
+  initChatbot(
+    {
+      apiHost: "https://llminabox.criticalfutureglobal.com",
+      chatflowId: "f0269cb6-fe4d-4f4e-b1dc-d4da241b97e2",
+      assistant: {
+        name: "digital voice",
+        description: "CEO of Tigercomm",
+        welcomeMessage: "Hello! How can I help you?",
+        avatar: {
+          staticUrl:
+            "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y"
+        }
+      },
+      ui: {
+        foregroundColor: "#2D3748",
+        backgroundColor: "#E2E8F0",
+        backgroundColorBody: "#CBD5E0"
+      }
+    },
+    document.getElementById("llminabox"),
+    "full"
   );
-
-  const utils = await import("../base/utils.js");
-
-  const newSection = await utils.createNewSection(chatbot, {
-    avatarImage,
-    avatarVideo,
-    themeFront,
-    themeBack,
-  });
-
-  newSection.scrollIntoView({ behavior: "smooth" });
-
-  utils.urlPreview();
 })();
