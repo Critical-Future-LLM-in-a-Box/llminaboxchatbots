4b446fc4ec0f1f4d1e2a53320d48613d2fccfc81
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2025-05-08 18:21:02 +0300

    Update reducer.ts

--- src/context/reducer.ts
+++ src/context/reducer.ts
@@ -81,8 +81,7 @@ export const chatReducer = (draft: ChatData, action: ChatActions) => {
         timestamp: new Date().toISOString()
       }
     ];
-    console.log(newChatId);
-    console.log(draft.session.chatId);
+
     localStorage.setItem(storageKey, JSON.stringify({...draft, session: {...draft.session, chatId: newChatId}}));
   }
 
