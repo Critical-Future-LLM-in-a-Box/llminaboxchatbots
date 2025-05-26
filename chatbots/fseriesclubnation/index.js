6ccb883547ef1c3892c29ba71a60f501428bb6be
mohamedtharwat000 <mohamed.tharwat.alazab@gmail.com>
2025-05-26 13:15:04 +0300

    Update index.js

--- chatbots/fseriesclubnation/index.js
+++ chatbots/fseriesclubnation/index.js
@@ -1,40 +1,39 @@
-await (async () => {
-  const apiHost = "https://llm.criticalfutureglobal.com";
-  const chatflowid = "713ac6ba-4a32-40a0-9408-89478be4337b";
-  const themeFront = "#212121";
-  const themeBack = "#e2deea";
-  const avatarIcon =
-    "https://critical-future-llm-in-a-box.github.io/llminaboxchatbots/Avatars/fs/joe.png";
-  const avatarImage =
-    "https://critical-future-llm-in-a-box.github.io/llminaboxchatbots/Avatars/fs/joe.gif";
-  const avatarVideo =
-    "https://critical-future-llm-in-a-box.github.io/llminaboxchatbots/Avatars/fs/joe.mp4";
+import { initChatbot } from "../../dist/llminaboxChat.js";
+(async () => {
+  // Create the container after the header with the correct new name
+  const header = document.querySelector("#header-outer");
+  const container = document.createElement("div");
+  container.id = "llminabox-fseries";
+  header && header.after(container);
 
-  if (location.href === "https://fseriesclubnation.com/") {
-    const chatbot = document.createElement("flowise-fullchatbot");
-    document.querySelector("#header-outer").after(chatbot);
-
-    (await import("../base/web.js")).default.initFull(
-      (await import("./config.js")).generateConfig({
-        apiHost,
-        chatflowid,
-        themeFront,
-        themeBack,
-        avatarIcon
-      })
-    );
-
-    const utils = await import("../base/utils.js");
-
-    await utils.createNewSection(chatbot, {
-      avatarImage,
-      avatarVideo,
-      themeFront,
-      themeBack
-    });
-
-    // await utils.urlPreview(chatbot);
-
-    await utils.ttsSupport(chatbot, "en-US-AndrewNeural");
-  }
+  initChatbot(
+    {
+      apiHost: "https://llminabox.criticalfutureglobal.com",
+      chatflowId: "27b4cb43-e79c-4508-a512-4c88fe0c5991",
+      chatDataId: 12345,
+      assistant: {
+        name: "JOE",
+        description: "Fseries Assistant",
+        welcomeMessage: "Hello! How can I help you?",
+        avatar: {
+          staticUrl:
+            "https://critical-future-llm-in-a-box.github.io/llminaboxchatbots/Avatars/fs/joe.png",
+          liveUrl:
+            "https://critical-future-llm-in-a-box.github.io/llminaboxchatbots/Avatars/fs/joe.gif",
+          videoUrl:
+            "https://critical-future-llm-in-a-box.github.io/llminaboxchatbots/Avatars/fs/joe.mp4"
+        }
+      },
+      ui: {
+        sidebar: true,
+        width: "100%",
+        height: "600px",
+        foregroundColor: "#212121",
+        backgroundColor: "#e2deea",
+        bodyBackgroundColor: "#e2deea"
+      }
+    },
+    document.getElementById("llminabox-fseries"),
+    "full"
+  );
 })();
